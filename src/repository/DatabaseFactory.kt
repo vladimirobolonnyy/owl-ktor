ff09663b586cc0ea2bea5677784303463d1a0fa7
Vladimir Obolonnyy <v.obolonnyy@alpacapital.ru>
2021-02-01 02:45:41 +0300

    fixing database

--- src/repository/DatabaseFactory.kt
+++ src/repository/DatabaseFactory.kt
@@ -19,7 +19,7 @@ object DatabaseFactory {
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+        config.jdbcUrl = System.getenv("JDBC_DATABASE_URL") ?: System.getenv("DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
